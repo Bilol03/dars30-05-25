f64c9582321aeff5f253f09b9f2656562cf76cf1
Bilol03 <madiyarovbilol78@gmail.com>
2025-11-19 16:39:33 +0500

    get a user

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const { readFile, writeFile } = require('./utils.js')
 
+
 const app = express()
 app.use(express.json())
 
@@ -19,7 +20,9 @@ app.post('/users', (req, res) => {
 	if (!(body.lastName && body.firstName && body.phone && body.age))
 		res.json({
 			message: "Ma'lumot to'liq kiritilmagan",
-		})
+	})
+	let user = users.find(el => el.phone == body.phone)
+	if(user) return res.json({message: "Bu telefon raqam mavjud"})
 	body.id = users[users.length - 1].id + 1
 	users.push(body)
 	writeFile('./users.json', users)
@@ -27,6 +30,22 @@ app.post('/users', (req, res) => {
 		message: 'OK',
 		status: 200,
 		users,
+	})
+})
+
+
+app.get('/users/:id', (req, res)=> {
+	let userId = req.params.id
+
+	let users = readFile('./users.json')
+	let user = users.find(el => el.id == userId)
+	if(!user) return res.json({
+		message: "User not found",
+		status: 404,
+	})
+	res.json({
+		message: "Success",
+		user
 	})
 })
 
